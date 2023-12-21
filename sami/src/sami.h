4b50c293c29147c8e067cfe755550f733ee87313
Ed Gallyot <edgallyot@gmail.com>
2023-12-21 23:44:51 +0000

    adding in some comments

--- sami/src/sami.h
+++ sami/src/sami.h
@@ -7,16 +7,24 @@
 #include "src/engine/engine.h"
 #include "utils/types.h"
 
+/** @brief creates a sami_engine
+ *
+ * @returns a pointer to a sami engine.
+ * NULL if not allocated.
+ **/
 sami_engine* sami_create_engine();
 
-/*
- * Returns -1 if unsuccessful and 
- * returns 0 if successful.
+/** @brief destroys the sami_engine
  *
  * This function can only fail on a null pointer
  * being passed.
  *
  * If free fails it is marked throw, so that 
  * will more than likely be a SIG. TODO: What is it?
- */
+ *
+ * @param the engine to destroy
+ *
+ * @returns i32 -1 if unsuccessful and 
+ * returns 0 if successful.
+ **/
 i32 sami_destroy_engine(sami_engine* engine);
